d638ce403c698d804c2ce98b8e451b25f92a1dbb
CristianTambaco <168015193+CristianTambaco@users.noreply.github.com>
2024-09-18 10:05:42 -0500

    Add files via upload

---
+++ operadores lógicos comparar dos números.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+//main(){
+//	bool esta_lleno = true;
+ //   cout << "Hay espacio? " <<(!esta_lleno ? "si" : "no") <<endl;
+    
+ //   return 0;
+
+//}
+
+
+
+main(){
+	
+	int num1=0;
+	int num2=0;
+	
+	cout<<"** Comparar dos numeros**"<<endl;
+	cout<<" Ingrese numero A"<<endl;
+	cin>>num1;
+	cout<<" Ingrese numero B"<<endl;
+	cin>>num2;
+	
+	cout<<" A y B son iguales? "<<((num1==num2)?"iguales": "diferentes") << endl;
+}
+
+
+
+
+
+
+
